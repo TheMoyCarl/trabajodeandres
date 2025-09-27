e55698c1ff8f5d993de33e42a1baeb6af7cb813f
TheMoyCarl <cemoyac@academia.usbbog.edu.co>
2025-09-26 20:46:30 -0500

    jwt y model

---
+++ config/jwt.py
@@ -0,0 +1,6 @@
+import os
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+JWT_TOKEN_LOCATION = ["headers"]
+JWT_ACCESS_TOKEN_EXPIRES = 3600
+JWT_HEADER_NAME = "Authorization"
+JWT_HEADER_TYPE = "Bearer"

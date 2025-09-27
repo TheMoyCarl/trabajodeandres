1c23e269f3bf35e22673b9b1057725eccd4c491d
TheMoyCarl <cemoyac@academia.usbbog.edu.co>
2025-09-26 21:03:09 -0500

    librerias faltantes

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from flask import Flask
+from config.jwt import *
 from controllers.libreria_controller import book_bp
+from controllers.user_controller import user_bp, register_jwt_error_handlers
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 
+# Configurar JWT
+app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
+app.config['JWT_TOKEN_LOCATION'] = JWT_TOKEN_LOCATION
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = JWT_ACCESS_TOKEN_EXPIRES
+app.config['JWT_HEADER_NAME'] = JWT_HEADER_NAME
+app.config['JWT_HEADER_TYPE'] = JWT_HEADER_TYPE
+
+jwt = JWTManager(app)
+
+# Registrar blueprints
 app.register_blueprint(book_bp)
+app.register_blueprint(user_bp)
+
+# Registrar manejadores personalizados de error JWT
+register_jwt_error_handlers(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
